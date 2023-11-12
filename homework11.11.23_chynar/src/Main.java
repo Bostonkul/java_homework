8cafec8659ec0bbe851b9ded5879a13704571490
Bostonkul <150093397+Bostonkul@users.noreply.github.com>
2023-11-12 03:13:44 +0100

    Add files via upload
    я попробовала разные методы ввода)

---
+++ homework11.11.23_chynar/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("моя фамилия Bostonkulova");
+        System.out.println("");
+        System.out.print("моя фамилия Bostonkulova");
+        System.out.println("моя фамилия Bostonkulova");
+        System.out.printf("моя фамилия %s", "Bostonkulova");
+    }
+}
